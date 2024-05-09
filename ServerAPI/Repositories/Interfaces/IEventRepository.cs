57f9b57d540c608b69a2781c28b511fc81fa9086
Marcus Houmark Larsen <eaa23mhla@students.eaaa.dk>
2024-05-09 13:21:22 +0200

    gg

--- ServerAPI/Repositories/Interfaces/IEventRepository.cs
+++ ServerAPI/Repositories/Interfaces/IEventRepository.cs
@@ -3,6 +3,7 @@ namespace ServerAPI.Repositories.Interfaces
 {
     public interface IEventRepository
     {
+        //yeet
         public List<Event> GetAllEvents();
         public void AddEvent(Event Event);
         public void RemoveEventByID(int eventID);
