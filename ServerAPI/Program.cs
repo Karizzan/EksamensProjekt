5c246bc189a6076e4bd480acef86dbc4bf85a29d
Hudayfa <hhw3103@hotmail.com>
2024-05-16 08:40:08 +0200

    Hudayfa lavede CSS

--- ServerAPI/Program.cs
+++ ServerAPI/Program.cs
@@ -159,6 +159,15 @@ namespace ServerAPI
 			var c = new EventRepository();
 			var d = new ParentChildRepository();
 
+			//Admin admin = new Admin();
+			//admin.AdminID = 1;
+			//admin.AdminName = "Admin1";
+			//admin.Username = "admin1";
+			//admin.Password = "123";
+			
+			
+			//a.AddAdmin(admin);
+
 			
 		/*	foreach (var item in parents)
 			{
