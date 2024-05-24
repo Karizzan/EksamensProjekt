3278f3faff580ccc31f5b2f260d4a7af8e8271b0
Marcus Houmark Larsen <eaa23mhla@students.eaaa.dk>
2024-05-24 13:23:39 +0200

    Css mangler, men alt andet skulle gerne virke

--- Core/Models/Event.cs
+++ Core/Models/Event.cs
@@ -1,4 +1,4 @@
-﻿    using MongoDB.Bson.Serialization.Attributes;
+﻿using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson;
 using System;
 using System.Collections.Generic;
@@ -8,19 +8,20 @@ using System.Threading.Tasks;
 
 namespace Core.Models
 {
-    public class Event
-    {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public ObjectId Id { get; set; }
-        public int EventID { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
-        public int WeekNumber { get; set; }
-        public string Location { get; set; }
-        public string Workdays { get; set; }
-        public List<Application> Applications { get; set; } = new();
+	public class Event
+	{
+		[BsonId]
+		[BsonRepresentation(BsonType.ObjectId)]
+		public ObjectId Id { get; set; }
+		public int EventID { get; set; }
+		public DateTime StartDate { get; set; }
+		public DateTime EndDate { get; set; }
+		public int WeekNumber { get; set; }
+		public string Location { get; set; }
+		public string Workdays { get; set; }
+		public List<Application> Applications { get; set; } = new();
+		public string FacebookGroup { get; set; } = "";
 
 
-    }
+	}
 }
